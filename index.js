b604f49e6ee1ea2132e7174a43702cbad8e77bcf
Guilherme <gui.lima2304@gmail.com>
2021-07-22 23:09:22 -0300

    finalizando api node alura

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = customExpress();
+conexao.connect((erro) => {
+    if(erro) {
+        console.log(erro);
+    } else {
+        console.log('Connected');
+        
+        Tabelas.init(conexao);
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
-
-// app.get('/', (req, res) => res.send('Servidor rodando, tudo ok'))
+        const app = customExpress();
+        
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
+    }
+});
 
