3a8302b63dd3f95298ffbeb51a18fca48afa7270
Guilherme <gui.lima2304@gmail.com>
2021-09-09 22:15:53 -0300

    configurando arquitetura do projeto e trabalhando com promises

---
+++ infraestrutura/database/queries.js
@@ -0,0 +1,16 @@
+const conexao = require('./conexao');
+
+//valor default do parametro eh vazio
+const executaQuery = (query, parametros = '') => {
+
+    //promise recebe como parametro uma funcao de callback 
+    return new Promise((resolve, reject) => {
+        conexao.query(query, parametros, (erros, resultados, campos) => {
+            if(erros) {
+                reject(erros);
+            } else {
+                resolve(resultados);
+            }
+        });
+    });
+}
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,4 +1,4 @@
-const conexao = require('../infraestrutura/conexao');
+const conexao = require('../infraestrutura/database/conexao');
 const moment = require('moment');
 const { default: axios } = require('axios');
 
